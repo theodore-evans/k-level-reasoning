c692dc812cfaefcf1c98c2ddb22637b24d6aa1c2
= <=>
2019-12-06 20:33:18 +0100

    built makefiles using CMake

---
+++ bin/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/config.cpp.o"
+  "CMakeFiles/app.dir/main.cpp.o"
+  "CMakeFiles/app.dir/matrix.cpp.o"
+  "CMakeFiles/app.dir/player.cpp.o"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bin/CMakeFiles/k-level-reasoning.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/k-level-reasoning.dir/config.cpp.o"
+  "CMakeFiles/k-level-reasoning.dir/main.cpp.o"
+  "CMakeFiles/k-level-reasoning.dir/matrix.cpp.o"
+  "CMakeFiles/k-level-reasoning.dir/player.cpp.o"
+  "k-level-reasoning"
+  "k-level-reasoning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/k-level-reasoning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
