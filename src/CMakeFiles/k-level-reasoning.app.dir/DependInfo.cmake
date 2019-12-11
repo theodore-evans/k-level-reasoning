c832935fee375425a11d80ad210f5fae470f1182
= <=>
2019-12-11 15:20:49 +0100

    renamed model directory to src

---
+++ src/CMakeFiles/k-level-reasoning.app.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/theoevans/k-level-reasoning/model/config.cpp" "/Users/theoevans/k-level-reasoning/model/CMakeFiles/k-level-reasoning.app.dir/config.cpp.o"
+  "/Users/theoevans/k-level-reasoning/model/main.cpp" "/Users/theoevans/k-level-reasoning/model/CMakeFiles/k-level-reasoning.app.dir/main.cpp.o"
+  "/Users/theoevans/k-level-reasoning/model/matrix.cpp" "/Users/theoevans/k-level-reasoning/model/CMakeFiles/k-level-reasoning.app.dir/matrix.cpp.o"
+  "/Users/theoevans/k-level-reasoning/model/player.cpp" "/Users/theoevans/k-level-reasoning/model/CMakeFiles/k-level-reasoning.app.dir/player.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/local/Cellar/jsoncpp/1.9.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/k-level-reasoning.app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/k-level-reasoning.app.dir/config.cpp.o"
+  "CMakeFiles/k-level-reasoning.app.dir/main.cpp.o"
+  "CMakeFiles/k-level-reasoning.app.dir/matrix.cpp.o"
+  "CMakeFiles/k-level-reasoning.app.dir/player.cpp.o"
+  "k-level-reasoning.app"
+  "k-level-reasoning.app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/k-level-reasoning.app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
